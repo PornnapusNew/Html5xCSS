44e4daed44dda6e4cb50e588ac69f0331fa9f236
PornnapusNew <73879636+PornnapusNew@users.noreply.github.com>
2021-07-01 00:19:58 +0700

    go

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//alert("Hello Newwy!")
+
+var x = 10;
+let y;
+const z = 15;
+x = 10;
+y = x;
+console.log(x, " ", y, " ", z);
+x = 20;
+
+console.log(x);
+console.log(z);
+
+var name = "John";
+var lastName = "Dou";
+var a = "20";
+var b = "10";
+console.log(name + lastName);
+console.log(name + x);
+console.log(a + b);
+console.log(parseInt(a) + parseInt(b));
